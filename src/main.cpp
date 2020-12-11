80edab0784b99ef07a43554960b6267cdb224f02
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-12-11 17:35:08 +0000

    implement todo 1

--- src/main.cpp
+++ src/main.cpp
@@ -56,11 +56,29 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
 
+    float start_x, start_y, end_x, end_y;
+
+    std::cout << "Type start_x: ";
+    std::cin >> start_x;
+    std::cout << std::endl;
+
+    std::cout << "Type start_y: ";
+    std::cin >> start_y;
+    std::cout << std::endl;
+
+    std::cout << "Type end_x: ";
+    std::cin >> end_x;
+    std::cout << std::endl;
+
+    std::cout << "Type end_y ";
+    std::cin >> end_y;
+    std::cout << std::endl;
+
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
